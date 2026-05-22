5c9b9c867ad763d2a5f3634a4e8d65bc90e92804
Lucian Raileanu <lucian@raileanu.ca>
2026-05-22 10:05:28 -0400

    updated author name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=['autodir']),
     version="1.0.0",
     description="Automatically switch directories, simply.",
-    author="HyperSource",
+    author="heysailboat",
     install_requires=['pathlib'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pathlib'],
